a67c7c384b90380c7afafba533510537e54d0c10
Marcio Bezerra <bezerradeveloper@gmail.com>
2022-02-17 11:10:23 -0300

    Inflection de veiculo

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,4 +2,5 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'funcao', 'funcoes'
   inflect.irregular 'departamento', 'departamentos'
   inflect.irregular 'motorista', 'motoristas'
+  inflect.irregular 'veiculo', 'veiculos'
 end
