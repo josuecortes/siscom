774ca7d11d4d51021a3087b79ac0d5de40d550bc
Marcio Bezerra <bezerradeveloper@gmail.com>
2022-02-21 19:37:20 -0300

    inflection de passageiro

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -4,4 +4,5 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'motorista', 'motoristas'
   inflect.irregular 'veiculo', 'veiculos'
   inflect.irregular 'requisicao_transporte', 'requisicao_transportes'
+  inflect.irregular 'passageiro', 'passageiros'
 end
