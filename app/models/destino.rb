d373a0e880ff8080532c9c5064c830cf7482cc63
Josue Cortes <josue.cortes@consultaremedios.com.br>
2022-12-21 11:00:34 -0300

    credentials and bug in destino.rb

--- app/models/destino.rb
+++ app/models/destino.rb
@@ -2,6 +2,6 @@ class Destino < ApplicationRecord
   belongs_to :requisicao_transporte, optional: true
   belongs_to :user, optional: true
 
-  validate_presence_of :cep, :numero, :descricao
+  validates_presence_of :cep, :numero, :descricao
 
 end
